64a37be0d6ffcd86770ac47f6842f9efa5ea63ac
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-25 11:00:39 +0530

    added data binding

--- src/app/emp-view/emp-view.component.ts
+++ src/app/emp-view/emp-view.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-view.component.css']
 })
 export class EmpViewComponent {
-
+  data = [{
+"name":"Abhishek",
+"designation":"SDE",
+"salary":2000,
+"age ":24,
+"email":"abhishdsek@ok.com",
+},
+{
+  "name":"Aijo",
+  "designation":"SDE",
+  "salary":21000,
+  "age ":24,
+  "email":"abhidfshek@ok.com",
+  },
+  {
+    "name":"Akshay",
+    "designation":"SDE",
+    "salary":22000,
+    "age ":24,
+    "email":"abhdsishek@ok.com",
+    },
+    {
+      "name":"Sarath",
+      "designation":"SDE",
+      "salary":23000,
+      "age ":24,
+      "email":"absahishek@ok.com",
+      }
+]
 }
